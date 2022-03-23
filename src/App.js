7dde313e368f0b38958d949e2f7dd71d4243408f
Logan Willis <logan.willis@perficient.com>
2022-03-23 12:34:05 -0500

    App.js cleaned

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 
-import { useContext, useState } from "react"
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from './data/FeedbackData'
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import About from './pages/About';
 import AboutIconLink from './components/AboutIconLink';
 import { FeedbackProvider } from './context/FeedbackContext';
-import FeedbackContext from "./context/FeedbackContext";
+
 
 export default function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
     return (
       <FeedbackProvider>
         <Router>
